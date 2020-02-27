41dc9550bf87ebdf8414ec0ef6ab1bb00cd6c08b
Vasily <50536980+Totaiu@users.noreply.github.com>
2020-02-27 17:36:12 +0900

    Add files via upload

---
+++ PrMd1/PrMd1/PrMd1.cpp
@@ -0,0 +1,28 @@
+// PrMd1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    char ch;
+    std::cout << "write string: ";
+    getline(std::cin, str);
+    std::cout << "write the char by which the string will be divided:  ";
+    std::cin >> ch;
+
+    for(int i = 0; i < str.length(); i++) 
+    {
+        if(str[i] == ch) 
+        {
+            std::cout << str[i] << std::endl;
+        }
+        else 
+        {
+            std::cout << str[i];
+        }
+    }
+}
+
